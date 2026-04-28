f575851f3b6ab3eed7d6704ac65e02a856c9f277
Sossyknave <bpeterson123490@gmail.com>
2026-04-28 13:26:32 -0400

    feat: added development tools for UDF compilation

--- fluentRocky8Packages.sh
+++ fluentRocky8Packages.sh
@@ -11,3 +11,5 @@ sudo dnf install glibc.i686 alsa-lib at-spi2-atk at-spi2-core atk avahi-libs cai
 sudo dnf install brotli bzip2-libs cyrus-sasl-lib expat fontconfig freetype glib2 glibc gmp gnutls keyutils-libs krb5-libs libICE libSM libX11 libX11-xcb libXau libXext libcom_err libcurl libffi libidn2 libnghttp2 libpng libpsl libselinux libssh libtasn1 libunistring libuuid libxcb libxcrypt libxkbcommon libxkbcommon-x11 nettle openldap openssl-libs p11-kit pcre pcre2 xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm zlib -y
 ## packages needed for fluent
 sudo dnf install audit-libs brotli bzip2-libs compat-hwloc1 compat-openssl10 cyrus-sasl-lib elfutils-libelf freetype gtk2 jbigkit-libs libX11 libXau libXdmcp libXext libXft libXtst libatomic libcap-ng libcurl libfontenc libgcc libglvnd-glx libibverbs libicu50 libnghttp2 libnl3 libpciaccess libpng libpsl librdmacm libssh libstdc++ libtiff libxshmfence ncurses-libs nss-softokn nss-softokn-freebl numactl-libs openldap pam perl-devel ucx xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xz-libs zlib -y
+## packages needed for UDF's
+sudo dnf groupinstall "Development Tools" -y
